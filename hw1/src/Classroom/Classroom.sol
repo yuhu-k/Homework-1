c2dee77dae54b504e7d49ee48793582e8e1f9462
Yuhu-K <kyh08436.cs@gmail.com>
2024-03-22 02:30:59 +0800

    update q3

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    bool private registed = false;
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        if (!registed) {
+            registed = true;
+            return 1000;
+        } else {
+            return 123;
+        }
     }
 }
 
@@ -17,13 +23,28 @@ interface IClassroomV2 {
 
 contract StudentV2 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        bool checker = isEnrolledInClassroom(msg.sender);
+        if (!checker){
+            return 1000;
+        } else {
+            return 123;
+        }
+    }
+
+    function isEnrolledInClassroom(address classroomAddress) public view returns (bool) {
+        IClassroomV2 classroom = IClassroomV2(classroomAddress);
+        return classroom.isEnrolled();
     }
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        if (gasleft() > 7000){
+            return 1000;
+        }else{
+            return 123;
+        }
     }
+
 }
